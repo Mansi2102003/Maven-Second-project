50eb22d3a7da1558a4f65ebf0f6d3f9ebaefe906
Mansi Thakkar <mthakkar@logilite.com>
2025-09-22 14:21:10 +0530

    "Simple calculator is created"

---
+++ src/main/java/com/mansi/SecondProject/Calculator.java
@@ -0,0 +1,16 @@
+package com.mansi.SecondProject;
+
+public class Calculator {
+	 public static int multiply(int a, int b) {
+		 return a*b;
+	 }
+	 public static int substraction(int a, int b) {
+		 return a-b;
+	 }
+	 public static int addition(int a, int b) {
+		 return a+b;
+	 }
+	 public static int division(int a, int b) {
+		 return a/b;
+	 }
+}
